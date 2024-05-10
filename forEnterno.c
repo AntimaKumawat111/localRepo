fed2cc223c4ac2165b4cbc9795beb32f90e7171d
AntimaKumawat <aktank810>
2024-05-10 17:09:38 +0530

    Add initial files

---
+++ forEnterno.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a no\n");
+    scanf("%d", &number);
+    printf("The number is %d\n", number);
+
+    return 0;
+}
---
+++ printName.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("My name is Antima\n");
+    return 0;
+}
